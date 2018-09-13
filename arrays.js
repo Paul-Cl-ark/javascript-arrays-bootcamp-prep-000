6ec5b571ae20ef9c0d25d6af89513fe9842a5942
Paul Clark <clark.paul123@gmail.com>
2018-09-13 21:26:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-console.log(array, index);
+(array, index);
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
